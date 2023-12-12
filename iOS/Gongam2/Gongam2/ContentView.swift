af71fc25d57df58dc9a9691bb2499d24e9bc1d83
Bucky <bucky5683@gmail.com>
2023-12-12 14:19:15 +0900

    iOS 추가

---
+++ iOS/Gongam2/Gongam2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gongam2
+//
+//  Created by 김서연 on 2023/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/Gongam2/Gongam2/Gongam2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gongam2App.swift
+//  Gongam2
+//
+//  Created by 김서연 on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct Gongam2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
