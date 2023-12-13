3fe74a9dab61da44a7125d521fa56a2128d26728
UhDgayeong <gygy3686@naver.com>
2023-12-13 16:17:25 +0900

    feat : 메인
    메인 : 랭킹 뷰 추가

--- AOS/gongam/app/src/main/java/com/example/gongam/MainActivity.kt
+++ AOS/gongam/app/src/main/java/com/example/gongam/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.gongam
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -17,14 +16,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -35,7 +36,6 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import com.example.gongam.ui.theme.GongamTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                     PreviewMain()
                 }
@@ -167,7 +167,7 @@ fun ContentsTitleView(title: String, showMoreButton: Boolean) {
                 color = colorResource(id = R.color.gray_scale2),
                 textDecoration = TextDecoration.Underline,
                 modifier = Modifier
-                    .padding(top = 30.dp, start = 40.dp, bottom = 2.dp)
+                    .padding(top = 15.dp, start = 40.dp, bottom = 2.dp)
             )
             Spacer(modifier = Modifier.weight(1f))
             if (showMoreButton) {
@@ -194,8 +194,8 @@ fun TimeButton(icon: String, title: String) {
                 .height(48.dp)
                 .shadow(
                     elevation = 10.dp,
-                    spotColor = colorResource(id = R.color.shadow_gray),
-                    ambientColor = colorResource(id = R.color.shadow_gray),
+                    spotColor = colorResource(id = R.color.shadow_gray3),
+                    ambientColor = colorResource(id = R.color.shadow_gray3),
                     shape = RoundedCornerShape(size = 10.dp)
                 )
                 .fillMaxWidth()
@@ -258,6 +258,212 @@ fun TimeButton(icon: String, title: String) {
     }
 }
 
+@Composable
+fun RankingView() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(37.dp))
+        Box(
+//                Modifier.fillMaxHeight()
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(61.dp)
+                    .height(215.dp)
+                    .padding(start = 7.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                colorResource(id = R.color.main_gray_70alpha),
+                                colorResource(id = R.color.main_gray)
+                            ),
+                            startY = 0f,
+                            endY = 215f
+                        ),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            ) {
+
+            }
+            Box(
+                modifier = Modifier
+                    .width(233.dp)
+//                        .fillMaxHeight(),
+            ) {
+
+            }
+            Box(
+//                    Modifier.fillMaxHeight(),
+            ) {
+                // 평균 공부시간 ----------------------------------------
+                Row(
+                    modifier = Modifier
+                        .height(215.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .shadow(
+                                ambientColor = colorResource(id = R.color.shadow_gray2),
+                                spotColor = colorResource(id = R.color.shadow_gray2),
+                                elevation = 5.dp,
+                                shape = RoundedCornerShape(size = 10.dp)
+                            ),
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .width(68.dp)
+                                .height(11.dp)
+                                .background(
+                                    color = colorResource(id = R.color.gray_scale1),
+                                    shape = RoundedCornerShape(10.dp)
+                                )
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(11.dp))
+                    Box(
+                        modifier = Modifier
+                            .shadow(
+                            ambientColor = colorResource(id = R.color.shadow_gray2),
+                            spotColor = colorResource(id = R.color.shadow_gray2),
+                            elevation = 5.dp,
+                            shape = RoundedCornerShape(size = 10.dp)
+                            )
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .height(18.dp)
+                                .background(
+                                    color = colorResource(id = R.color.gray_scale1),
+                                    shape = RoundedCornerShape(10.dp)
+                                )
+                                .padding(horizontal = 12.dp, vertical = 0.dp),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "평균 공부시간 : ",
+                                    color = Color.White,
+                                    fontWeight = FontWeight(500),
+                                    fontSize = 10.sp,
+                                    textAlign = TextAlign.Center,
+                                )
+                                Text(
+                                    text = "9999:99:99",
+                                    color = Color.White,
+                                    fontWeight = FontWeight(700),
+                                    fontSize = 10.sp,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
+                    }
+                }
+                // 유저 등수 ----------------------------------------
+                Column(
+                    modifier = Modifier
+                        .height(215.dp),
+                ) {
+                    // TODO :: 유저의 등수에 따라 Spacer 높이 조절
+                    Spacer(modifier = Modifier.height(130.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = Modifier.shadow(
+                                ambientColor = colorResource(id = R.color.shadow_gray2),
+                                spotColor = colorResource(id = R.color.shadow_gray2),
+                                elevation = 5.dp,
+                                shape = RoundedCornerShape(size = 10.dp)
+                            ),
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(68.dp)
+                                    .height(11.dp)
+                                    .background(
+                                        color = colorResource(id = R.color.blue_scale1),
+                                        shape = RoundedCornerShape(10.dp)
+                                    )
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(11.dp))
+                        Box(
+                            modifier = Modifier.shadow(
+                                ambientColor = colorResource(id = R.color.shadow_gray2),
+                                spotColor = colorResource(id = R.color.shadow_gray2),
+                                elevation = 5.dp,
+                                shape = RoundedCornerShape(size = 10.dp)
+                            ),
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .height(46.dp)
+                                    .background(
+                                        color = colorResource(id = R.color.blue_scale1),
+                                        shape = RoundedCornerShape(10.dp)
+                                    )
+                                    .padding(horizontal = 12.dp, vertical = 0.dp),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        text = "OOO님의 등수",
+                                        color = colorResource(id = R.color.main_gray),
+                                        fontWeight = FontWeight(400),
+                                        fontSize = 12.sp,
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Spacer(modifier = Modifier.width(13.dp))
+                                    Text(
+                                        text = "999+",
+                                        color = colorResource(id = R.color.main_gray),
+                                        fontWeight = FontWeight(700),
+                                        fontSize = 20.sp,
+                                        textAlign = TextAlign.Center,
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "평균보다 ",
+                fontSize = 12.sp,
+                color = colorResource(id = R.color.main_gray),
+                fontWeight = FontWeight(700)
+            )
+            Text(
+                text = "999:99:99",
+                fontSize = 15.sp,
+                color = colorResource(id = R.color.blue_scale2),
+                fontWeight = FontWeight(700)
+            )
+            Text(
+                text = " 만큼 덜 공부했어요!",
+                fontSize = 12.sp,
+                color = colorResource(id = R.color.main_gray),
+                fontWeight = FontWeight(700)
+            )
+        }
+        Spacer(modifier = Modifier.height(31.dp))
+    }
+
+}
+
 @Preview
 @Composable
 fun PreivewTopView() {
@@ -276,7 +482,13 @@ fun PreviewTimerView() {
     ContentsTitleView("타이머", false)
 }
 
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
+@Composable
+fun PreviewRankingView() {
+    RankingView()
+}
+
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun PreviewMain() {
     Column(
@@ -284,13 +496,17 @@ fun PreviewMain() {
             .background(
                 color = Color.White
             )
+            .verticalScroll(rememberScrollState())
     ) {
         TopView()
+        Spacer(modifier = Modifier.height(15.dp))
         ContentsTitleView("타이머", false)
         Spacer(modifier = Modifier.height(13.5.dp))
         TimeButton(icon = "⏰", title = "타이머")
         TimeButton(icon = "⏱️", title = "스톱워치")
         Spacer(modifier = Modifier.height(42.5.dp))
         ContentsTitleView("랭킹", true)
+        RankingView()
+        Spacer(modifier = Modifier.height(15.dp))
     }
 }
