c675a3f755d6298f0e07c1be7b671d094fa8af12
Bucky <bucky5683@gmail.com>
2024-01-05 13:07:53 +0900

    swiftui 다시 도전

---
+++ iOS/newGongam2/newGongam2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newGongam2
+//
+//  Created by 김서연 on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/newGongam2/newGongam2/newGongam2App.swift
@@ -0,0 +1,17 @@
+//
+//  newGongam2App.swift
+//  newGongam2
+//
+//  Created by 김서연 on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct newGongam2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
