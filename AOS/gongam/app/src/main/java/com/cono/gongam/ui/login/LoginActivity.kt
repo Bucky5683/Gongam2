bc45435325231c84ca8ddd341c1590aab5fc31af
UhDgayeong <gygy3686@naver.com>
2024-01-09 17:29:55 +0900

    feat : 로그인
    로그인 : Google 로그인 버튼 생성

--- AOS/gongam/app/src/main/java/com/cono/gongam/ui/login/LoginActivity.kt
+++ AOS/gongam/app/src/main/java/com/cono/gongam/ui/login/LoginActivity.kt
@@ -9,19 +9,46 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cono.gongam.R
 import com.cono.gongam.ui.login.ui.theme.GongamTheme
 import com.cono.gongam.ui.main.MainActivity
 import com.cono.gongam.ui.splash.SplashImage
@@ -38,7 +65,7 @@ class LoginActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White,
                 ) {
                     LoginScreen(onLoginSuccess = { user ->
                         startActivity(Intent(this@LoginActivity, MainActivity::class.java))
@@ -53,13 +80,25 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen(onLoginSuccess: (FirebaseUser) -> Unit) {
-    SplashImage()
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterHorizontally),
-        verticalAlignment = Alignment.CenterVertically,
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Hello")
+        Spacer(modifier = Modifier.weight(1f))
+        SplashImage()
+        Spacer(modifier = Modifier.weight(2f))
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 113.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
         GoogleLoginButton(onLoginSuccess = onLoginSuccess)
+//        Spacer(modifier = Modifier.height())
 //        GoogleSignIn
     }
 }
@@ -95,20 +134,60 @@ fun GoogleLoginButton(onLoginSuccess: (FirebaseUser) -> Unit) {
         }
     }
 
-    Button(
-        onClick = {
-            val providers = arrayListOf(
-                AuthUI.IdpConfig.GoogleBuilder().build()
-            )
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(16.dp)
+            .background(Color.White)
+            .clickable {
+                val providers = arrayListOf(
+                    AuthUI.IdpConfig
+                        .GoogleBuilder()
+                        .build()
+                )
 
-            val signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build()
+                val signInIntent = AuthUI
+                    .getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build()
 
-            startForResult.launch(signInIntent)
-        }
+                startForResult.launch(signInIntent)
+            },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 3.dp
+        ), // Add shadow here
+        shape = RoundedCornerShape(10.dp), // You can adjust corner shape as needed
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
     ) {
-        Text("Google 로그인")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(15.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_google),
+                contentDescription = "Google Logo",
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(15.dp)) // Add space between image and text
+            Text(
+                text = "Google 로그인",
+                fontSize = 17.sp,
+                fontWeight = FontWeight(500),
+                modifier = Modifier.align(Alignment.CenterVertically),
+                color = colorResource(id = R.color.shadow_gray4)
+            )
+        }
     }
+
+}
+
+// ------------------------------------ Previews ------------------------------------
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen(onLoginSuccess = {})
 }
--- AOS/gongam/app/src/main/java/com/cono/gongam/ui/splash/SplashActivity.kt
+++ AOS/gongam/app/src/main/java/com/cono/gongam/ui/splash/SplashActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +30,7 @@ class SplashActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White,
                 ) {
                     LaunchedEffect(true) {
                         delay(3000) // 3초 지연
