ff4560d54561de30171e0944e979aefa815b2ad5
UhDgayeong <gygy3686@naver.com>
2024-01-10 10:16:04 +0900

    feat : 상태바 색상 설정
    Theme 설정 변경

--- AOS/gongam/app/src/main/java/com/cono/gongam/ui/login/ui/theme/Theme.kt
+++ AOS/gongam/app/src/main/java/com/cono/gongam/ui/login/ui/theme/Theme.kt
@@ -10,10 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import com.cono.gongam.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -57,7 +58,8 @@ fun GongamTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            val statusBarColor = ContextCompat.getColor(view.context, R.color.white)
+            window.statusBarColor = statusBarColor
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
