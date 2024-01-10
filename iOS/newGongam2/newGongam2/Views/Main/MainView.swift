3d7610e864293a14a43989a3b8211b23e18938c9
Bucky <bucky5683@gmail.com>
2024-01-10 16:07:28 +0900

    MainView 대충 화면 구성완료

--- iOS/newGongam2/newGongam2/Views/Main/MainView.swift
+++ iOS/newGongam2/newGongam2/Views/Main/MainView.swift
@@ -9,45 +9,90 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var userData: UserData
-    @State var timerButtonClicked: Bool = false
-    @State var stopwatchButtonClicked: Bool = false
-    @State var rankMoreButtonClicked: Bool = false
-    @State var myReportMoreButtonClicked: Bool = false
     
     var body: some View {
         NavigationView{
-            NavigationLink(
-                destination: TimerView(),
-                isActive: $timerButtonClicked,
-                label: {
-                    EmptyView()
-                }
-            )
-            NavigationLink(
-                destination: TimerView(),
-                isActive: $stopwatchButtonClicked,
-                label: {
-                    EmptyView()
-                }
-            )
-            NavigationLink(
-                destination: TimerView(),
-                isActive: $rankMoreButtonClicked,
-                label: {
-                    EmptyView()
-                }
-            )
-            NavigationLink(
-                destination: TimerView(),
-                isActive: $myReportMoreButtonClicked,
-                label: {
-                    EmptyView()
-                }
-            )
             VStack{
+                Text("MainView!")
                 MainHeaderView()
-                MainTimerView(timerButtonClicked: $timerButtonClicked, stopwatchButtonClicked: $stopwatchButtonClicked)
+                //MARK: 타이머
+                VStack {
+                    Text("타이머")
+                    TimersButtonView(isTimer: true)
+                    TimersButtonView(isTimer: false)
+                }
+                
+                //MARK: 랭크
+                VStack{
+                    MainSubHeaderView(isRank: true)
+                    NavigationLink(destination: RankView(), label: {
+                        MainRankChartView()
+                    })
+                    Text("평균보다 만큼 덜 공부했어요")
+                }
+                
+                //MARK: 마이리포트
+                VStack{
+                    MainSubHeaderView(isRank: false)
+                    NavigationLink(destination: ReportView(), label: {
+                        MainMyReportGridView()
+                    })
+                    Text("이번 주에 평균 만큼 공부했어요!")
+                }
             }
+        }.navigationBarHidden(true)
+    }
+}
+
+
+struct TimersButtonView: View {
+    var isTimer: Bool
+    var body: some View {
+        HStack{
+            if isTimer{
+                Text("⏰")
+                Text("타이머")
+                NavigationLink(destination: TimerView(), label: {
+                    HStack{
+                        Text("GO")
+                        Image("goButtonIcon")
+                    }
+                })
+            } else {
+                Text("⏱️")
+                Text("스톱워치")
+                NavigationLink(destination: TimerView(), label: {
+                    HStack{
+                        Text("GO")
+                        Image("goButtonIcon")
+                    }
+                })
+            }
+        }
+    }
+}
+
+struct MainSubHeaderView: View {
+    var isRank: Bool = false
+    var body: some View {
+        HStack{
+            Spacer()
+            if isRank {
+                Text("랭크")
+            } else {
+                Text("마이 리포트")
+            }
+            Spacer()
+            if isRank {
+                NavigationLink(destination: RankView(), label: {
+                    Text("더보기 >")
+                })
+            } else {
+                NavigationLink(destination: ReportView(), label: {
+                    Text("더보기 >")
+                })
+            }
+            Spacer()
         }
     }
 }
---
+++ iOS/newGongam2/newGongam2/Views/Report/ReportView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportView.swift
+//  newGongam2
+//
+//  Created by 김서연 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportView()
+}
