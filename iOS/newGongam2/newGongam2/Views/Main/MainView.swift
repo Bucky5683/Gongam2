a4d97db84560b37cad4e89232995c13d6460b015
Bucky <bucky5683@gmail.com>
2024-01-10 17:37:59 +0900

    popup 추가

--- iOS/newGongam2/newGongam2/Views/Main/MainView.swift
+++ iOS/newGongam2/newGongam2/Views/Main/MainView.swift
@@ -6,39 +6,84 @@
 //
 
 import SwiftUI
+import PopupView
 
 struct MainView: View {
     @EnvironmentObject var userData: UserData
+    @State var showingPopup = false
     
     var body: some View {
         NavigationView{
-            VStack{
-                Text("MainView!")
-                MainHeaderView()
-                //MARK: 타이머
-                VStack {
-                    Text("타이머")
-                    TimersButtonView(isTimer: true)
-                    TimersButtonView(isTimer: false)
-                }
-                
-                //MARK: 랭크
+            ZStack{
                 VStack{
-                    MainSubHeaderView(isRank: true)
-                    NavigationLink(destination: RankView(), label: {
-                        MainRankChartView()
-                    })
-                    Text("평균보다 만큼 덜 공부했어요")
+                    Button{
+                        print("MainHeader Profile Image Clicked!")
+                        showingPopup = true
+                    }label: {
+                        AsyncImage(url: URL(string: userData.profileImageURL)){ image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }.frame(width: 30, height: 30)
+                    }
+                    MainHeaderView()
+                    //MARK: 타이머
+                    VStack {
+                        Text("타이머")
+                        TimersButtonView(isTimer: true)
+                        TimersButtonView(isTimer: false)
+                    }
+                    
+                    //MARK: 랭크
+                    VStack{
+                        MainSubHeaderView(isRank: true)
+                        NavigationLink(destination: RankView(), label: {
+                            MainRankChartView()
+                        })
+                        Text("평균보다 만큼 덜 공부했어요")
+                    }
+                    
+                    //MARK: 마이리포트
+                    VStack{
+                        MainSubHeaderView(isRank: false)
+                        NavigationLink(destination: ReportView(), label: {
+                            MainMyReportGridView()
+                        })
+                        Text("이번 주에 평균 만큼 공부했어요!")
+                    }
                 }
-                
-                //MARK: 마이리포트
+            }.popup(isPresented: $showingPopup){
                 VStack{
-                    MainSubHeaderView(isRank: false)
-                    NavigationLink(destination: ReportView(), label: {
-                        MainMyReportGridView()
-                    })
-                    Text("이번 주에 평균 만큼 공부했어요!")
-                }
+                    HStack{
+                        VStack{
+                            Text("응애")
+                            Text("asdifjaweoifjwe@gmail.com")
+                            Text("목표 공부시간 99:99:99")
+                        }
+                        AsyncImage(url: URL(string: userData.profileImageURL)){ image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }.frame(width: 80, height: 80)
+                    }
+                    Button {
+                        print("Clicked Helper!!")
+                    } label: {
+                        Text("도움말")
+                    }
+                    Button {
+                        print("Clicked 이용약관!!")
+                    } label: {
+                        Text("이용약관")
+                    }
+                }.background(.whiteFFFFFF)
+            } customize: {
+                $0
+                    .type(.floater())
+                    .position(.topTrailing)
+                    .animation(.snappy)
+                    .closeOnTapOutside(true)
+                    .backgroundColor(.black.opacity(0.5))
             }
         }.navigationBarHidden(true)
     }
