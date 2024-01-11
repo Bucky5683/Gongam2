ff058f04bba843c0c3242dbab9e97fa69d120616
UhDgayeong <gygy3686@naver.com>
2024-01-11 14:53:19 +0900

    feat : Utils
    DateUtils, SharedPreferencesUtil 생성

---
+++ AOS/gongam/app/src/main/java/com/cono/gongam/utils/SharedPreferencesUtil.kt
@@ -0,0 +1,39 @@
+package com.cono.gongam.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.cono.gongam.data.User
+
+class SharedPreferencesUtil(context: Context) {
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+    fun saveUser(user: User) {
+        editor.putString("email", user.email)
+        editor.putInt("goalStudyTime", user.goalStudyTime ?: 0)
+        editor.putString("lastUpdateDate", user.lastUpdateDate)
+        editor.putString("name", user.name)
+        editor.putString("profileImageURL", user.profileImageURL)
+        editor.putInt("stopwatchStudyTime", user.stopwatchStudyTime ?: 0)
+        editor.putInt("timerStudyTime", user.timerStudyTime ?: 0)
+        editor.putInt("todayStudyTime", user.todayStudyTime ?: 0)
+        editor.apply()
+    }
+
+    fun getUser(): User {
+        return User(
+            email = sharedPreferences.getString("email", ""),
+            goalStudyTime = sharedPreferences.getInt("goalStudyTime", 0),
+            lastUpdateDate = sharedPreferences.getString("lastUpdateDate", ""),
+            name = sharedPreferences.getString("name", ""),
+            profileImageURL = sharedPreferences.getString("profileImageURL", ""),
+            stopwatchStudyTime = sharedPreferences.getInt("stopwatchStudyTime", 0),
+            timerStudyTime = sharedPreferences.getInt("timerStudyTime", 0),
+            todayStudyTime = sharedPreferences.getInt("todayStudyTime", 0)
+        )
+    }
+
+    fun clearUser() {
+        editor.clear().apply()
+    }
+}
