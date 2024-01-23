5f5be51f16f7942b4891301e02afdee76f3364ae
UhDgayeong <gygy3686@naver.com>
2024-01-23 09:40:22 +0900

    feat : 메인
    마이 리포트 주 단위 공부량 및 평균값 세팅

--- AOS/gongam/app/build.gradle.kts
+++ AOS/gongam/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.cono.gongam"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- AOS/gongam/app/src/main/java/com/cono/gongam/utils/DateUtils.kt
+++ AOS/gongam/app/src/main/java/com/cono/gongam/utils/DateUtils.kt
@@ -1,14 +1,26 @@
 package com.cono.gongam.utils
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 object DateUtils {
+    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+
     fun getCurrentDate(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        val currentDate = Date()
-        return dateFormat.format(currentDate)
+//        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+//        val currentDate = Date()
+//        return dateFormat.format(currentDate)
+
+        return LocalDate.now().format(dateFormatter)
+    }
+
+    fun getStartOfWeek(): LocalDate {
+        return LocalDate.now().with(DayOfWeek.SUNDAY).minusDays(7)
+    }
+
+    fun getEndOfWeek(): LocalDate {
+        return LocalDate.now().with(DayOfWeek.SATURDAY)
     }
 }
